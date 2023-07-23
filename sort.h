5bc30aaad22f08a93bb317d54546e864ccbfada2
ahmedkhalid90h <ahmedkhalid90h@gmail.com>
2023-07-23 11:18:15 +0300

    goo

--- sort.h
+++ sort.h
@@ -25,6 +25,17 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 listint_t *swap_left(listint_t *node, listint_t **list);
 void selection_sort(int *array, size_t size);
-
+void quick_sort(int *array, size_t size);
+void quickSort(int *array, int first, int last, int size);
+int lomuto_partition(int *array, int first, int last, int size);
+void swap(int *a, int *b);
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
 
 #endif
